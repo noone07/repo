b3e603f86eb375682f7144eb3eb597a99676be70
noone07 <72711627+noone07@users.noreply.github.com>
2024-11-08 11:57:15 +0200

    Update netdata_deployment.sh
    Signed-off-by: noone07 <72711627+noone07@users.noreply.github.com>

--- netdata_deployment.sh
+++ netdata_deployment.sh
@@ -6,11 +6,11 @@ CLAIM_TOKEN="xDNhD-0jGfjqIs6lVnHytA9AykYdFqA9lpRgoDYu1Z2RF6fdCT4HPHQEgTQUlWcN-tB
 CLAIM_ROOMS="cc835736-4b90-44d0-acd0-ea15ec3a38c2"
 CLAIM_URL="https://app.netdata.cloud"
 
-# Download the Netdata kickstart script to /tmp
-wget -O /tmp/netdata-kickstart.sh "$NETDATA_SCRIPT_URL"
+# Download the Netdata kickstart script to /tmp as Jenkins user
+sudo -u jenkins wget -O /tmp/netdata-kickstart.sh "$NETDATA_SCRIPT_URL"
 
-# Run the script with the specified options
-bash /tmp/netdata-kickstart.sh --stable-channel \
+# Run the script with the specified options as Jenkins user
+sudo -u jenkins bash /tmp/netdata-kickstart.sh --stable-channel \
   --claim-token "$CLAIM_TOKEN" \
   --claim-rooms "$CLAIM_ROOMS" \
   --claim-url "$CLAIM_URL"
