321ccaa3f78e8e229fbf047a0f2e66aed092d525
noone07 <72711627+noone07@users.noreply.github.com>
2024-11-08 11:41:40 +0200

    Create netdata_deployment.sh
    Signed-off-by: noone07 <72711627+noone07@users.noreply.github.com>

---
+++ netdata_deployment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define the URL, token, rooms, and claim URL
+NETDATA_SCRIPT_URL="https://get.netdata.cloud/kickstart.sh"
+CLAIM_TOKEN="xDNhD-0jGfjqIs6lVnHytA9AykYdFqA9lpRgoDYu1Z2RF6fdCT4HPHQEgTQUlWcN-tBihrgLmPa-GNddsfy76a3SwXIrr3xHK1nSLA88Lw0LVhRt-jI0KjmtgPQ8HZ0wKima7Z8"
+CLAIM_ROOMS="cc835736-4b90-44d0-acd0-ea15ec3a38c2"
+CLAIM_URL="https://app.netdata.cloud"
+
+# Download the Netdata kickstart script to /tmp
+wget -O /tmp/netdata-kickstart.sh "$NETDATA_SCRIPT_URL"
+
+# Run the script with the specified options
+bash /tmp/netdata-kickstart.sh --stable-channel \
+  --claim-token "$CLAIM_TOKEN" \
+  --claim-rooms "$CLAIM_ROOMS" \
+  --claim-url "$CLAIM_URL"
